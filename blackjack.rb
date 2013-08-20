ef8138f63b30233b5f5f9a6d8c57f6a0c309603f
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2013-08-20 14:50:22 -0400

    deck dealing

--- blackjack.rb
+++ blackjack.rb
@@ -1,17 +1,39 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
+class Blackjack
+  
+  SUITS = ['♠', '♣', '♥', '♦']
+  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-SUITS = ['♠', '♣', '♥', '♦']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-def build_deck
-  deck = []
-
-  SUITS.each do |suit|
-    VALUES.each do |value|
-      deck.push(value + suit)
-    end
+  def build_deck
+      @deck = []
+      @player=[]
+      @dealer=[]
+      SUITS.each do |suit|
+        VALUES.each do |value|
+        @deck.push(value + suit)
+        end
+      end
+      @shuffled_deck = @deck.shuffle
+    self.init
   end
 
-  deck.shuffle
+  def init
+    2.times{@player << @shuffled_deck.pop }
+    2.times{@dealer << @shuffled_deck.pop }
+   print @player
+  end
+
 end
+
+
+game = Blackjack.new
+game.build_deck
+
+
+#deal two card out to player
+#store cards in player 1
+#show cards to player
+
+#choose hit or stay
